3e41242c51daf1daf6523d215bbe8ffcad524cdc
jdnaranjo1411 <112138324+jdnaranjo1411@users.noreply.github.com>
2024-05-23 21:05:13 -0500

    init

---
+++ src/components/Organism/gameboard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="balls">
+    <div class="game-board">
+          <square :x="n" :y="n" :occupied="false" :selected="false" v-for="n in 81" :key="n"/>
+        </div>
+  </div>
+</template>
+
+<script setup>
+import square from '../Atoms/square.vue';
+import { onMounted, ref } from 'vue';
+const useEffect = onMounted;
+
+
+const board = ref([]); // Array para almacenar el tablero
+
+useEffect(() => {
+  for (let i = 0; i < 9; i++) {
+    const row = [];
+    for (let j = 0; j < 9; j++) {
+      // Asignar coordenadas x e y a cada cuadrícula
+
+      row.push({ i, j });
+    }
+    board.value.push(row);
+  }
+  console.log(board.value);
+});
+</script>
+
+<style scoped>
+
+
+.balls {
+  display: flex;
+  justify-content: center;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: repeat(9, minmax(50px, 1fr));
+}
+
+.row {
+  display: flex;
+}
+  
+</style>
---
+++ src/components/Templares/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- Componente del juego -->
+    <!-- <Game /> -->
+
+    <!-- Barra de herramientas -->
+    <Toolbar />
+
+    <gameboard />
+    <!-- Jugador -->
+  </div>
+</template>
+
+<script>
+// Importa los componentes
+import Game from '@/components/Organism/game.vue'
+import Toolbar from '@/components/Organism/toolbar.vue' 
+import gameboard from '@/components/Organism/gameboard.vue'
+export default {
+  components: {
+    Game,
+    Toolbar,
+    gameboard
+  }
+}
+</script>
+
+<style src="./AppStyles.css"></style>
+
